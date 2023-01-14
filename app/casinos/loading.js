3046f7a058e6e93b67c662579fffea0f6078570b
Fran <fran.siner@gmail.com>
2023-01-14 21:28:42 +0100

    Removing loading in cached pages

--- app/casinos/loading.js
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-    return (
-        <div className="grid place-items-center pt-20">
-            <progress className="progress w-56"></progress>
-        </div>
-    )
-}
