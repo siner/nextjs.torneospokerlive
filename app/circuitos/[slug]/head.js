1ddcdd1203355a2f531625c9d5103b086be3ebc8
Fran <fran.siner@gmail.com>
2023-06-03 17:52:26 +0200

    Añadidos metas que faltaban y corregido layout

--- app/circuitos/[slug]/head.js
+++ app/circuitos/[slug]/head.js
@@ -4,10 +4,20 @@ import { getTour } from '../../../lib/prisma'
 export default async function Head({ params }) {
     const circuito = await getTour(params.slug)
     var title = 'Circuito no encontrado - Torneos Poker Live'
+    var description = `Circuito de poker ${circuito.name}`
     if (circuito.name) title = `${circuito.name} - Torneos Poker Live`
     return (
         <>
             <title>{title}</title>
+            <meta property="description" content={description} />
+            <meta property="og:image" content={circuito.logo} />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="twitter:image" content={circuito.logo} />
+            <meta property="twitter:card" content="summary" />
+            <meta property="twitter:title" content={title} />
+            <meta property="twitter:description" content={description} />
+
             <DefaultTags />
         </>
     )
--- app/torneos/[slug]/head.js
+++ app/torneos/[slug]/head.js
@@ -1,19 +1,37 @@
 import DefaultTags from '../../defaultTags'
 import { getTournament } from '../../../lib/prisma'
+import { localeDateString } from '../../../lib/utils'
 
 export default async function Head({ params }) {
     var torneo = await getTournament(params.slug)
+    let date = localeDateString(torneo.date)
 
     var casino = ''
     if (torneo.casino) {
         casino = torneo.casino.name
     }
     var title = 'Torneo no encontrado - Torneos Poker Live'
+    var description = `Torneo de poker ${torneo.name} en el ${casino} el ${date}`
+
     if (torneo.name) title = `${torneo.name} ${casino} - Torneos Poker Live`
 
     return (
         <>
             <title>{title}</title>
+            <meta property="description" content={description} />
+            <meta
+                property="og:image"
+                content="https://torneospokerlive.com/logo.png"
+            />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta
+                property="twitter:image"
+                content="https://torneospokerlive.com/logo.png"
+            />
+            <meta property="twitter:card" content="summary" />
+            <meta property="twitter:title" content={title} />
+            <meta property="twitter:description" content={description} />
             <DefaultTags />
         </>
     )
