eee1b1b1e091a4204fbd336f35fc93ad18a2f21c
Fran <fran.siner@gmail.com>
2023-08-18 10:58:44 +0200

    Mejorado minirow

--- components/tournament/MiniRowTournament.jsx
+++ components/tournament/MiniRowTournament.jsx
@@ -5,6 +5,8 @@ export default function MiniRowTournament(props) {
     const backgroundColor = torneo.casino.color
     const textColor = getTextColor(backgroundColor)
     let datetorneo = new Date(torneo.date)
+    const hour = datetorneo.getHours()
+    const minutes = datetorneo.getMinutes()
     let today = new Date()
     today.setHours(0, 0, 0, 0)
     let opacity = datetorneo < today ? '0.7' : '1'
@@ -22,27 +24,32 @@ export default function MiniRowTournament(props) {
                 <div className="name w-full text-left text-xs">
                     <a
                         href={'/torneos/' + torneo.slug}
-                        className="flex justify-start items-center content-center"
+                        className="flex justify-between items-center content-center"
                     >
-                        {casino && (
-                            <Image
-                                src={`https://wsrv.nl/?url=${torneo.casino.logo}&w=100&h=40&fit=contain`}
-                                width={20}
-                                height={8}
-                                alt={'Logo ' + torneo.casino.name}
-                                className="mr-2 w-5 h-3"
-                            />
-                        )}
-                        {event && torneo.evento && (
-                            <Image
-                                src={`https://wsrv.nl/?url=${torneo.evento.tour.logo}&w=100&h=100&fit=contain`}
-                                width={20}
-                                height={20}
-                                alt={'Icono ' + torneo.evento.name}
-                                className="mr-2 h-5"
-                            />
-                        )}
-                        {torneo.name}
+                        <div className="flex justify-start content-center items-center">
+                            {casino && (
+                                <Image
+                                    src={`https://wsrv.nl/?url=${torneo.casino.logo}&w=100&h=40&fit=contain`}
+                                    width={100}
+                                    height={40}
+                                    alt={'Logo ' + torneo.casino.name}
+                                    className="mr-2 w-8"
+                                />
+                            )}
+                            {event && torneo.evento && (
+                                <Image
+                                    src={`https://wsrv.nl/?url=${torneo.evento.tour.logo}&w=100&h=100&fit=contain`}
+                                    width={20}
+                                    height={20}
+                                    alt={'Icono ' + torneo.evento.name}
+                                    className="mr-2 w-8"
+                                />
+                            )}
+                            <span>{torneo.name}</span>
+                        </div>
+                        <span className="text-right">
+                            {hour}:{String(minutes).padStart(2, '0')}
+                        </span>
                     </a>
                 </div>
             </div>
