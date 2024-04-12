5776b93e9b472db544e38db82e8a72509651486c
Fran Moreno <fran.siner@gmail.com>
2024-04-12 15:51:57 +0200

    Added SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -35,6 +36,7 @@ export default function RootLayout({
           </main>
         </div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
