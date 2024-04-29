3f730ba13c83e0e37e8de22de35278cabf2c484b
Fran Moreno <fran.siner@gmail.com>
2024-04-29 23:02:01 +0200

    Removed vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -39,8 +37,6 @@ export default function RootLayout({
             {children}
           </main>
         </div>
-        <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
